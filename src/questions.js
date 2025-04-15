2a5cf1652d193d485d56615ed3d35f3c39bfd34f
Melis Yıldız <melisyildiz707@gmail.com>
2025-04-15 18:24:19 +0300

    fix:

--- src/questions.js
+++ src/questions.js
@@ -1,23 +1,28 @@
-// Quiz questions data
+// Quiz soruları ve cevapları
 const questions = [
   {
     question: "Çin Seddini oluşturan taşlar birbirine ne ile tutturulmuştur?",
     options: ["Bambu Harcı", "Anne Duası", "Pirinç Unu", "Noodle"],
     correctAnswer: "Pirinç Unu",
     media: "/pictures/cin-seddi.jpg",
+    index: 0,
+    total: 10
   },
   {
     question: "İlk Pamuk şekeri bulan kişinin mesleği nedir?",
     options: ["Gıda Mühendisi", "Diş Doktoru", "Ev Hanımı", "Güzellik Uzmanı"],
     correctAnswer: "Diş Doktoru",
     media: "/pictures/pamuk.jpg",
+    index: 1,
+    total: 10
   },
   {
-    question:
-      "Tarkan'ın 'Hüp' klibini izledikten sonra gaza gelip 'Tarkan keşke beni hüpletseydi' diye açıklamda bulunan kişi kimdir?",
+    question: "Tarkan'ın 'Hüp' klibini izledikten sonra gaza gelip 'Tarkan keşke beni hüpletseydi' diye açıklamda bulunan kişi kimdir?",
     options: ["Gülben Ergen", "Hülya Avşar", "Harika Avcı", "Sevtap Parman"],
     correctAnswer: "Gülben Ergen",
     media: "/pictures/tarkan.jpg",
+    index: 2,
+    total: 10
   },
   {
     question: "Pteronofobi nedir?",
@@ -25,51 +30,61 @@ const questions = [
       "Yeşil ışık yanar yanmaz korna çalacak korkusu",
       "Fakir kalma korkusu",
       "Taksi bulamama korkusu",
-      "Kuş tüyüyle gıdıklanma korkusu",
+      "Kuş tüyüyle gıdıklanma korkusu"
     ],
     correctAnswer: "Kuş tüyüyle gıdıklanma korkusu",
     media: "/pictures/fobi.jpg",
+    index: 3,
+    total: 10
   },
   {
-    question:
-      "Ortalama ömürleri 5 yıl olan Japon balıklarının en uzun yaşayanı Tish, bütün istatistikleri alt üst ederek kaç yıl boyunca hayata tutunmayı başarmıştır?",
+    question: "Ortalama ömürleri 5 yıl olan Japon balıklarının en uzun yaşayanı Tish, bütün istatistikleri alt üst ederek kaç yıl boyunca hayata tutunmayı başarmıştır?",
     options: ["43", "78", "23", "99"],
     correctAnswer: "43",
     media: "/pictures/balik.jpg",
+    index: 4,
+    total: 10
   },
   {
-    question:
-      "90'lara damgasını vuran 'Bandıra Bandıra' şarkısının söz yazarı kimdir?",
+    question: "90'lara damgasını vuran 'Bandıra Bandıra' şarkısının söz yazarı kimdir?",
     options: ["Sezen Aksu", "Sibel Can", "Mustafa Sandal", "Bülent Ersoy"],
     correctAnswer: "Mustafa Sandal",
     media: "/pictures/bandira.jpg",
+    index: 5,
+    total: 10
   },
   {
-    question:
-      "Hangi şarkıcımız yine kendisi gibi şarkıcı olan sevgilisinden ayrıldıktan sonra tam evinin karşısındaki apartmanın tamamını kendi posteriyle kaplatmıştır?",
+    question: "Hangi şarkıcımız yine kendisi gibi şarkıcı olan sevgilisinden ayrıldıktan sonra tam evinin karşısındaki apartmanın tamamını kendi posteriyle kaplatmıştır?",
     options: ["Hande Yener", "Hadise", "Gülşen", "Simge"],
     correctAnswer: "Hadise",
     media: "/pictures/billboard.jpg",
+    index: 6,
+    total: 10
   },
   {
     question: "Antik Roma'da kadınlar parfüm olarak ne kullanıyordu?",
     options: ["Gül Suyu", "Bal", "Gladyatör Teri", "Kan"],
     correctAnswer: "Gladyatör Teri",
     media: "/pictures/parfum.jpg",
+    index: 7,
+    total: 10
   },
   {
     question: "T-Rex'in yaşayan en yakın akrabası aşağıdakilerden hangisidir?",
     options: ["İnekler", "Tavuklar", "Timsahlar", "Köpekler"],
     correctAnswer: "Tavuklar",
     media: "/pictures/trex.jpg",
+    index: 8,
+    total: 10
   },
   {
-    question:
-      "Her şeyin olduğu gibi mutluluğun da fobisi varmış. Bu fobiye ne ad verilir?",
+    question: "Her şeyin olduğu gibi mutluluğun da fobisi varmış. Bu fobiye ne ad verilir?",
     options: ["Çerofobi", "Euphobia", "Felicifobia", "Mutluluk Korkusu"],
     correctAnswer: "Çerofobi",
     media: "/pictures/fobi.jpg",
-  },
+    index: 9,
+    total: 10
+  }
 ];
 
 export default questions;
